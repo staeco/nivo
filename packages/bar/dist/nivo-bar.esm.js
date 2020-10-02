7305a9a958bbd4526ca35a9493400eca013f0538
Ben Tyler <ben@stae.co>
2020-10-02 09:48:17 -0700

    chore: update BarItem props

--- packages/bar/dist/nivo-bar.esm.js
+++ packages/bar/dist/nivo-bar.esm.js
@@ -579,7 +579,7 @@ ArrowItem.propTypes = {
   data: PropTypes.shape({
     id: PropTypes.string.isRequired,
     value: PropTypes.number.isRequired,
-    indexValue: PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(Date)]),
+    indexValue: PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(Date), PropTypes.number]),
     fill: PropTypes.string
   }).isRequired,
   x: PropTypes.number.isRequired,
